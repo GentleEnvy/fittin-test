fff6bd6c660c7bf2f7c14b79f2046c747e56aa6f
GentleEnvy <komarov.sergei163@gmail.com>
2022-07-10 11:41:49 +0300

    update CategoryFactory: supports update or create

--- app/categories/factories/categories.py
+++ app/categories/factories/categories.py
@@ -5,6 +5,9 @@ from app.categories.models import Category
 
 
 class _CategoryManagerType(Protocol):
+    def get(self, *, id: int) -> Category:
+        pass
+
     def create(self, *, id: int, name: str, parent: Category = None) -> Category:
         pass
 
@@ -14,14 +17,24 @@ class CategoryFactory:
         self.manager: _CategoryManagerType = Category.objects
 
     def create(self, category_dto: CategoryDTO) -> Category:
-        category = self.manager.create(id=category_dto.id, name=category_dto.name)
+        return self._create(category_dto)
+
+    def _create(self, category_dto: CategoryDTO, parent: Category = None) -> Category:
+        category = self._update_or_create(category_dto, parent)
         for child_dto in category_dto.children:
-            self._create_child(child_dto, category)
+            self._create(child_dto, category)
         return category
 
-    def _create_child(self, category_dto: CategoryDTO, parent: Category) -> None:
-        child = self.manager.create(
-            id=category_dto.id, name=category_dto.name, parent=parent
-        )
-        for child_dto in category_dto.children:
-            self._create_child(child_dto, child)
+    def _update_or_create(
+        self, category_dto: CategoryDTO, parent: Category = None
+    ) -> Category:
+        try:
+            category = self.manager.get(id=category_dto.id)
+            category.name = category_dto.name
+            category.parent = parent
+            category.save()
+        except Category.DoesNotExist:
+            category = self.manager.create(
+                id=category_dto.id, name=category_dto.name, parent=parent
+            )
+        return category
